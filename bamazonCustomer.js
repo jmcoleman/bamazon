8b5badb51ac390937ef03f344daaa963292f372d
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-08-04 16:11:07 -0400

    basic ordering

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,7 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var currencyFormatter = require('currency-formatter');
+const cTable = require('console.table');
 
 ///////////////////////////////////
 // variables
@@ -37,34 +38,34 @@ var askQuestion = function() {
             name: "productid",
             message: "What is the id of the product to purchase?"
         },
-        // find out what the quantity is
+        // find out how many
         {
             name: "units",
             message: "How many units?"
         }
     ]).then(function(answers) {
-        placeOrder(answers.productid, answers.units);
-        showItemsForSale();
+        fulfillOrder(parseInt(answers.productid), parseInt(answers.units));
     });
 }
 
 function showItemsForSale () {
     // select all products for sale with inventory on hand
-    connection.query("select item_id, product_name, price from products where stock_quantity > 0",
+    connection.query("select item_id, product_name, price, stock_quantity from products where stock_quantity > 0",
         function(err, res) {
             if (err) throw err;
-            // console.log(res);
+            console.table(res);
 
-            res.forEach((element, index) => {
-                // console.log(`Current index: ${index}`);
-                // console.log(element);
+            // res.forEach((element, index) => {
+            //     // console.log(`Current index: ${index}`);
+            //     // console.log(element);
          
-                console.log(
-                    " Item ID: " + element.item_id + "\n", 
-                    "Product Name: " + element.product_name + "\n", 
-                    "Price: " + currencyFormatter.format(element.price, { code: 'USD' }) + "\n"
-                );
-            });
+            //     console.log(
+            //         " Item ID: " + element.item_id + "\n", 
+            //         "Product Name: " + element.product_name + "\n", 
+            //         "Price: " + currencyFormatter.format(element.price, { code: 'USD' }) + "\n",
+            //         "Stock: " +  element.stock_quantity + "\n"
+            //     );
+            // });
 
             // console.log(
             //     "\n Item ID: " + res[0].item_id + "\n", 
@@ -76,34 +77,65 @@ function showItemsForSale () {
         });
 };
 
-function placeOrder(prod, quantity) {
-    console.log("in place order");
+function fulfillOrder(item, purchaseQty) {
+    // console.log("in fulfill order");
 
-    // if (checkInventoryStock(prod, quantity)) {
-    //     fullfillOrder();
-    // } else {
-    //     console.log("Not enough inventory on hand.");
-    // }
+    var query;
 
-    // notify user of purchase
+    ///////////////////
+    // check stock
+    ///////////////////
+    query = connection.query("select stock_quantity from products where item_id = ?",
+        item,
+        function(err, res) {
+            if (err) throw err;
+            
+            // console.log(res);
+            // console.log("current stock quantity is: " + res[0].stock_quantity);
+            // console.log("current purchase quantity is: " + purchaseQty);
 
+            if (res[0].stock_quantity >= purchaseQty && purchaseQty > 0) {
+
+                //////////////////////////
+                // decrease inventory
+                //////////////////////////
+                query = connection.query("update products set stock_quantity = ? where item_id = ?",
+                    [
+                        res[0].stock_quantity - purchaseQty,
+                        item
+                    ],
+                    function(err, res) {
+                        if (err) throw err;
+                        // console.log(res.affectedRows + " products updated!\n");
+                    });
+                
+                // console.log(query.sql);
+                showItemPrice(item,purchaseQty);
+
+            } else {
+                if (purchaseQty <= 0 || isNaN(purchaseQty)) {
+                    console.log("\nInvalid quantity.\n");
+                } else {
+                    console.log("\nThere is not enough inventory to purchase this item.\n");
+                }
+            }
+            showItemsForSale();
+        });
+
+    console.log(query.sql);
 };
 
-function checkInventoryStock (product, qty) {
-    var stockOnHand = false;
-
-    
-    // check if stock in on hand and whether there are enough units of it
-    if (haveEnough) {
-        stockOnHand = true;
-    }
-    return stockOnHand;
-
-};
-
-function fulfillOrder() {
-    // decrease stock by updating the SQL database to reflect the remaining quantity
+function showItemPrice(item, qty) {
+    var salesAmount;
 
     //  show the customer the total cost of their purchase
+    query = connection.query("select product_name, price from products where item_id = ?",
+        [item],
+        function(err, res) {
+            if (err) throw err;
+            salesAmount = res[0].price * qty;
+            console.log(`\nTotal cost for ${qty} unit(s) of ${res[0].product_name}: ` + currencyFormatter.format(salesAmount, { code: 'USD' }) + "\n");
+        });
+    console.log(query.sql);
 
 };
